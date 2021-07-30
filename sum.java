eea84d4883cd9d502728963346608d1dd670b116
Pankaj14449 <Pankaj14449@gmail.com>
2021-07-30 10:46:50 +0530

    Added sum java file

---
+++ sum.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+class sum
+{
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int firstNum = scan.nextInt();
+        int secondNum = scan.nextInt();
+
+        output = firstNum +secondNum;
+        System.output.println(output);
+
+    }
+}
